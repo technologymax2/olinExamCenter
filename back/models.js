1dbe9d9f95f658d73f314ac82969e8863b3eb025
technologymax2 <technologymax2@gmail.com>
2026-08-09 02:28:23 -0700

    Update models.js

--- back/models.js
+++ back/models.js
@@ -7,7 +7,9 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'teacher', 'student'], default: 'student' }
+    role: { type: String, enum: ['admin', 'teacher', 'student'], default: 'student' },
+    resetRequested: { type: Boolean, default: false },
+    resetTokenExpire: { type: Date, default: null }
 });
 
 const User = mongoose.model('User', userSchema);
